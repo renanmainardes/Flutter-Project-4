3dc5316b94065e5a7042a1d74a599a57c1fa25c7
renanmainardes <renanmainardes@outlook.com>
2022-07-24 17:13:16 -0300

    new commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:project_4/ui/home_page.dart';
+
+
+void main() async {
+  runApp(MaterialApp(home: HomePage(),
+  theme: ThemeData(hintColor: Colors.white)));
+  }
+  
